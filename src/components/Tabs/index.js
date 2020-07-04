e1b336bc53e82c588381b86375bd5c9664a0bc25
Marcos Lucas <marcosvaldeni@gmail.com>
2020-07-04 17:20:03 +0100

    tabs animations

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -9,9 +9,22 @@ import {
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function Tabs() {
+export default function Tabs({ translateY }) {
   return (
-    <Container>
+    <Container style={{
+      transform: [{
+        translateY: translateY.interpolate({
+          inputRange: [0, 380],
+          outputRange: [0, 30],
+          extrapolate: 'clamp',
+        })
+      }],
+      opacity:translateY.interpolate({
+        inputRange: [0, 380],
+        outputRange: [1, 0.1],
+        extrapolate: 'clamp',
+      }),
+    }}>
       <TabsContainer>
         <TabItem>
           <Icon name="person-add" size={24} color="#FFF" />
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -79,7 +79,7 @@ export default function Main() {
         </Content>
 
 
-      <Tabs />
+      <Tabs translateY={translateY}/>
     </Container>
   );
 }
